afe4289b43ef6c9213ac6becc0fc25cb5a2b6a86
HumairaHaslah <humairayyan7@gmail.com>
2026-06-27 14:48:08 +0800

    Add files via upload

---
+++ ADS Lab 2 Task 2.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+int median(vector<vector<int> > &mat) {
+    int n = mat.size(); //3
+    int m = mat[0].size(); // 3
+
+    // default INT_MAX and INT_MIN from <climits>
+    int minVal = INT_MAX, maxVal = INT_MIN;
+
+    //finding the min and max elements 
+    for (int i = 0; i < n ; i++) {
+        if (mat[i][0] < minVal) {
+            minVal = mat[i][0];
+        }
+        if (mat[i][m - 1] > maxVal) {
+            maxVal = mat[i][m - 1];
+        }
+    }
+
+    int desired = (n * m + 1) / 2; 
+    // 3 * 3  = 9+ 1 / 2 = 5
+    int lo = minVal, hi = maxVal;
+    while (lo < hi) {
+        int mid = lo + (hi - lo) / 2;
+        // 5
+        int count = 0;
+
+        //count elements smaller then or equal to mid
+        for (int i = 0; i < n; i++) {
+            count += upper_bound(mat[i].begin(), mat[i].end(), mid) - mat[i].begin();
+        }
+        
+        if (count < desired) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+            // hi = 5
+        }
+    }
+    return lo;
+}
+
+
+int main() {
+    vector<vector<int> > mat = {
+        {1, 3, 5},
+        {2, 6, 9},
+        {3, 6, 9}
+    };
+    cout << "Median: " << median(mat) << endl;
+    return 0;
+}
