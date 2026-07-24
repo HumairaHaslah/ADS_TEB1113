b27abb36cbb7b72f2369913472b74b862779c495
HumairaHaslah <humairayyan7@gmail.com>
2026-07-24 16:33:54 +0800

    Add files via upload

---
+++ Lab 8 Task 3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i]; 
+        int j = i - 1; 
+
+        while (j >= 0 && arr[j] > key) {
+            swap(arr[j + 1], arr[j]);
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {56, 72, 30, 15, 78, 54, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array (insertion sort): ";
+    printArray(arr, n);
+
+    return 0;
+}
